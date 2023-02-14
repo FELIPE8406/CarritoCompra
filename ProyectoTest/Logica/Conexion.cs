8bb57c1c9db5400fc1db79a89c34b4cb406cfed7
Felipe Valbuena <felipev8406@hotmail.com>
2023-02-13 20:35:50 -0500

    Agregar archivos de proyecto.

---
+++ ProyectoTest/Logica/Conexion.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProyectoTest.Logica
+{
+    public class Conexion
+    {
+        public static string CN = "Data Source = DESKTOP-C6I3D6D\\SQLEXPRESS; Initial Catalog = DB_CARRITO; Integrated Security = True";
+
+    }
+}
